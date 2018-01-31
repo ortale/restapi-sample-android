686eeebbc6b72de8c05cc5025c7a0b002b367afc
RODOLFO ORTALE <rodolfoortale@RODOLFOsMacBook.lan>
2018-01-31 20:26:02 +0000

    Created FormHelper

---
+++ app/src/main/java/br/com/rodolfoortale/brighthr/helper/FormHelper.java
@@ -0,0 +1,11 @@
+package br.com.rodolfoortale.brighthr.helper;
+
+/**
+ * Created by rodolfoortale on 31/01/2018.
+ */
+
+public class FormHelper {
+    public static void validateLogin(String login, String password) {
+        
+    }
+}
