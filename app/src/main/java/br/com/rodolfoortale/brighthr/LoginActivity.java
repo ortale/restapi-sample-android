81d661c5741fabaa22a402d61071bfb07a100fbb
RODOLFO ORTALE <rodolfoortale@RODOLFOsMacBook.lan>
2018-01-31 21:17:44 +0000

    Created validation rules

--- app/src/main/java/br/com/rodolfoortale/brighthr/LoginActivity.java
+++ app/src/main/java/br/com/rodolfoortale/brighthr/LoginActivity.java
@@ -2,12 +2,43 @@ package br.com.rodolfoortale.brighthr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import br.com.rodolfoortale.brighthr.helper.FormHelper;
 
 public class LoginActivity extends AppCompatActivity {
+    FormHelper formHelper;
+    EditText edtLogin;
+    EditText edtPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtLogin = findViewById(R.id.edt_login);
+        edtPassword = findViewById(R.id.edt_password);
+        btnLogin = findViewById(R.id.btn_login);
+
+        formHelper = FormHelper.getInstance(this);
+        formHelper.checkEmptyFields(edtLogin, edtPassword, btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        formHelper = FormHelper.getInstance(this);
+
+        if (formHelper.validateLogin(edtLogin, edtPassword)) {
+            
+        }
     }
 }
--- app/src/main/java/br/com/rodolfoortale/brighthr/helper/FormHelper.java
+++ app/src/main/java/br/com/rodolfoortale/brighthr/helper/FormHelper.java
@@ -1,11 +1,83 @@
 package br.com.rodolfoortale.brighthr.helper;
 
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+import br.com.rodolfoortale.brighthr.R;
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
 /**
  * Created by rodolfoortale on 31/01/2018.
  */
 
 public class FormHelper {
-    public static void validateLogin(String login, String password) {
+    private static FormHelper instance = null;
 
+    private Context context;
+    private SweetAlertDialog sweetAlertDialog;
+
+    private FormHelper(Context context) {
+        this.context = context;
+    }
+
+    public static FormHelper getInstance(Context context) {
+        if(instance == null) {
+            instance = new FormHelper(context);
+        }
+        return instance;
+    }
+
+    private boolean isValidPassword(String password) {
+        String pattern = "^(?=.*[0-9])(?=.*[A-Z]).{6,}$";
+
+        return password.matches(pattern);
+    }
+
+    public boolean validateLogin(EditText edtLogin, EditText edtPassword) {
+        String valLogin = edtLogin.getText().toString();
+        String valPassword = edtPassword.getText().toString();
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(valLogin).matches()) {
+            edtLogin.setError(context.getString(R.string.login_invalid_email));
+
+            return false;
+        }
+
+        else if (!isValidPassword(valPassword)) {
+            edtPassword.setError(context.getString(R.string.login_invalid_password));
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public void checkEmptyFields(EditText edtLogin, EditText edtPassword, final Button btnLogin) {
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String text = editable.toString();
+
+                if (!text.isEmpty()) {
+                    btnLogin.setEnabled(true);
+                }
+            }
+        };
+
+        edtLogin.addTextChangedListener(textWatcher);
+        edtPassword.addTextChangedListener(textWatcher);
     }
 }
